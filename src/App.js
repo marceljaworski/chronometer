4564ba5a694e15b54fe37ef8270cc078184aeef5
Marcelearn <jaworskimarcel@gmail.com>
2022-10-22 11:14:51 +0200

    styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import './App.css'
 
 function App() {
   const[time, setTime] = React.useState(0)
@@ -15,14 +16,27 @@ function App() {
     }
     return() => clearInterval(interval)
   },[timerOn])
+
   return (
     <div className="App">
-      <div>{time}</div>
       <div>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)},</span>
+        <span>{("0" +((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div>
+        {!timerOn && time === 0 && (
         <button onClick={()=> setTimeOn(true)}>Start</button>
+        )}
+        {timerOn && (
         <button onClick={()=> setTimeOn(false)}>Stop</button>
+        )}
+        {!timerOn && time !== 0 && (
         <button onClick={()=> setTimeOn(true)}>Resume</button>
-        <button onClick={()=> setTimeOn(0)}>Reset</button>
+        )}
+        {!timerOn && time > 0 && (
+        <button onClick={()=> setTime(0)}>Reset</button>
+        )}
       </div>
     </div>
   );
