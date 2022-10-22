b024aabc2519446df6aa102740ed79e72a588af8
Marcelearn <jaworskimarcel@gmail.com>
2022-10-22 09:10:45 +0200

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react"
+
 function App() {
   const[time, setTime] = React.useState(0)
   const[timerOn, setTimeOn] = React.useState(false)
-  
-  ReactDOM.useEffect(()=>{
 
-  },[])
+  React.useEffect(()=>{
+    let interval = null;
+    if(timerOn){
+      interval = setInterval(()=> {
+        setTime(prevTime => prevTime + 10) 
+      }, 10)
+    }else{
+      clearInterval(interval)
+    }
+    return() => clearInterval(interval)
+  },[timerOn])
   return (
     <div className="App">
       <div>{time}</div>
